50da2967450ffefec7f6604644dddaec872b1cab
hotaru7mmd <hotaru7mmd@outlook.com>
2025-12-27 03:15:38 -0700

    Update autoload.js

--- dist/autoload.js
+++ dist/autoload.js
@@ -56,7 +56,7 @@ function loadExternalResource(url, type) {
   // 配置选项的具体用法见 README.md
   initWidget({
     waifuPath: live2d_path + 'waifu-tips.json',
-    // cdnPath: 'https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/',
+    cdnPath: 'https://fastly.jsdelivr.net/gh/hotaru7mmd/lumine-live2d/',
     cubism2Path: live2d_path + 'live2d.min.js',
     cubism5Path: 'https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js',
     tools: ['hitokoto', 'asteroids', 'switch-model', 'switch-texture', 'photo', 'info', 'quit'],
